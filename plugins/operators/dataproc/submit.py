547e7dc1a6295fecc836cd0146f94a54097528ce
Castagnna <daniel.c4stagna@gmail.com>
2024-03-16 12:15:17 -0300

    upgrade submit operator

--- plugins/operators/dataproc/submit.py
+++ plugins/operators/dataproc/submit.py
@@ -8,18 +8,62 @@ from operators.dataproc.defaults import REGION, MAIN_PATH, EGG_PATH
 class SubmitPySparkJobOperator(DataprocSubmitPySparkJobOperator):
     @apply_defaults
     def __init__(
-        self, task_id: str, arguments: list, cluster_name: str, *args, **kwargs
+        self,
+        task_id: str,
+        cluster_name: str,
+        layer: str,
+        job_name: str,
+        env: str = "prd",
+        mode: str = "cluster",
+        datetime: str = None,
+        dry_run: bool = False,
+        noop: bool = False,
+        *args,
+        **kwargs
     ):
+
         super(SubmitPySparkJobOperator, self).__init__(
             task_id=task_id,
+            cluster_name=cluster_name,
+            arguments=self.__make_arguments(
+                layer, job_name, env, mode, datetime, dry_run, noop
+            ),
             main=MAIN_PATH,
             pyfiles=[
                 EGG_PATH,
                 MAIN_PATH,
             ],
-            arguments=arguments,
-            cluster_name=cluster_name,
             region=REGION,
             *args,
             **kwargs
         )
+
+    @staticmethod
+    def __make_arguments(
+        layer: str,
+        job_name: str,
+        env: str = "prd",
+        mode: str = "cluster",
+        dttm: str = None,
+        dry_run: bool = False,
+        noop: bool = False,
+    ):
+        arguments = [
+            layer,
+            job_name,
+            "--env",
+            env,
+            "--mode",
+            mode,
+        ]
+
+        if dttm:
+            arguments.extend(["--datetime", dttm])
+
+        if dry_run:
+            arguments.append("--dry-run")
+
+        if noop:
+            arguments.append("--noop")
+
+        return arguments
